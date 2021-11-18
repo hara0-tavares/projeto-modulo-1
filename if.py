c1b56f7141c0f83d4696e9e5c65aa01dc5d80073
Harão <harao.araujo@gmail.com>
2021-11-17 21:21:58 -0300

    primeiro versão do jogo

---
+++ if.py
@@ -0,0 +1,12 @@
+idade = int(input('Olá jogador seja bem vindo ao war, escolha um exercito 1 = war br , 2 = war chines , 3 = war eua: '))  
+if idade == 1 :
+    print('escolheu war br')
+elif idade == 2 :
+    print('escolheu war chines')
+elif idade == 3 :
+    print('escolheu war eua')
+else:
+    print('volte e escolha um exercito 1 , 2 , 3')
+
+
+
